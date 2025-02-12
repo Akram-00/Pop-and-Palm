0e465f0067f8856277086938def0eef1d2d3a351
Akram-00 <aliakram9789@gmail.com>
2025-02-12 19:14:54 +0530

    Contact us CTA added to each and every blog

--- src/components/BlogContent.jsx
+++ src/components/BlogContent.jsx
@@ -9,8 +9,10 @@ const BlogContent = ({ blog }) => {
             <h1 className='blog-title'>{blog.title}</h1>
             <div className='blog-hr' />
             {/* blogcontent */}
-            <blog.blogContent /> 
-            <button>Contact Us</button>
+            <blog.blogContent />
+            <a href='https://us.bigin.online/org868107012/forms/enquiry-form' target='_blank' rel="noopener noreferrer">
+                <button className='button-primary blog-redirect-btn' style={{marginTop:'2rem'}}>Contact Us</button>
+            </a>
         </section>
     )
 }
